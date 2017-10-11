af059d556ea592bb0dd216d45754df75e019a16d
Suraj Bhatia <surajbhatia.sb@gmail.com>
2017-10-10 20:34:47 -0400

    Minor

--- Practice/emailParser.py
+++ Practice/emailParser.py
@@ -9,7 +9,5 @@ for i in range(int(raw_input())):
 
         parseList = email.utils.parseaddr(raw_input())
 
-        # [a-zA-Z](\w|-|\.)*@[a-zA-Z]*\.[a-zA-Z]{0,3}$
-
         if bool(re.match('[a-zA-Z](\w|-|\.)*@[a-zA-Z]*\.[A-Za-z]{0,3}$', parseList[1])):
                 print email.utils.formataddr(parseList)
